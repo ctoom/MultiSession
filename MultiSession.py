d0a45f9378299777607c45067ec0b17fea5962e5
Artur Hnat <Artur.Hnat@gmail.com>
2019-04-27 21:08:19 +0200

    initial commit

--- MultiSession.py
+++ MultiSession.py
@@ -1,18 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import threading
+from threading import Thread
+from colorama import Fore, Back, Style
+from colorama import init
+from pyfiglet import figlet_format
+
+print(figlet_format(('rTunaboss'), font='big'))
+
+init(autoreset=True)
 
 
-url = input("What is the url: ")
-i = int(input("how many instances?"))
-for _ in range(i):
+
+url = input("\nEnter the url:\t")
+
+instances = int(input("\nHow many instances?\t"))
+
+def set_options():
+    # Initialize the webdriver
     chrome_options = Options()
     chrome_options.add_argument("--window-size=400,600")
-    driver = webdriver.Chrome(executable_path="/Users/arturhnat/Desktop/Coding/WebDrivers/chromedriver", chrome_options=chrome_options)
+    return chrome_options
+
+def initialize_driver(options, url):
+    driver = webdriver.Chrome(executable_path="/Users/arturhnat/Desktop/Coding/WebDrivers/chromedriver", options=options)
     driver.get(url)
 
+
+options = set_options()
+
+for i in range(instances):
+    initialize_driver(options=options, url=url)
+    print(Fore.GREEN + f"\nDriver n.{i+1} has been started.")
+
+
 while True:
-    close = input("Press 'q' when done:\t")
-    if close.lower() == "q":
-        driver.quit()
+    q = input(Fore.RED + "Enter 'Q' to exit")
+    if q.lower() == "q":
+        print("\nExiting...")
         break
